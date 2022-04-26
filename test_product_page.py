49ebd396b470e59de41981ee7f863e829facbfa3
Ada Shyrchanka <Ada.Shyrchanka@itechart-group.com>
2022-04-26 15:26:46 +0300

    Add tests for messages

--- test_product_page.py
+++ test_product_page.py
@@ -25,3 +25,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_book_name()
     product_page.should_be_book_price()
 
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)   
+    product_page.open()                     
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)   
+    product_page.open()  
+    product_page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)   
+    product_page.open()  
+    product_page.add_to_basket()
+    product_page.should_disappear()
